5305e7f378b9957cc212e742937538ca35c27d81
~ Yurii <locomoc.yt@gmail.com>
2025-06-24 19:44:51 +0300

    test s

--- src/main/java/com/polis/Calculator.java
+++ src/main/java/com/polis/Calculator.java
@@ -3,7 +3,8 @@ package com.polis;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;}
+        return a + b;
+    }
 
     public int subtract(int a, int b) {
         return a - b;
